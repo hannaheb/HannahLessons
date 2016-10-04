73d73b561a4d32284f0ce82c75543b7b5ea98b6f
Hannah <hannahebarker@icloud.com>
2016-10-04 09:13:59 -0400

    FIrst Lesson

---
+++ Hello python.py
@@ -0,0 +1,32 @@
+import random
+import sys
+import os
+
+print("Hello world")
+
+name = "Hannah"
+print(name)
+
+
+print("5 + 2 =",  5+2)
+print("233112 * 5489485 =",233112 * 5489485)
+
+print("Whats your movie do you like")
+
+print ("I like zoolander")
+
+quote = "\" Always remember you are unique"
+print (quote)
+print ("draw sphere")
+
+quote = "\" life is like a box of chocolates you never know what your gonna get"
+print(quote)
+
+grocery_list = ['juice', 'tomatoes', 'potatoes', 'bananas']
+print('First item', grocery_list[0])
+print(grocery_list)
+other_events = ['wash car','pick up kids','cash check']
+to_do_list = [other_events,grocery_list]
+print(to_do_list)sudo apt-get install xfswitch-plugin
+print((to_do_list[1][1]))sudo apt-get install xfswitch-plugin
+#commet 10:44 youtube derek benes python programing
